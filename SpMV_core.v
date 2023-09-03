cec3eee1339bb0481894efe26168799f1838f903
jeongyongbeom <jeong8310@naver.com>
2023-09-03 15:04:29 +0000

    core

--- SpMV_core.v
+++ SpMV_core.v
@@ -13,7 +13,7 @@ module SpMV_core(
 	input [7:0]			count,
 	input [135:0]		row_ptr,
 	
-	output reg 			o_done,
+	output  			o_done,
 	output [255:0]		o_register
 );	
 	parameter IDLE		= 3'b000;
@@ -21,6 +21,15 @@ module SpMV_core(
 	parameter REG_READ  = 3'b010;
 	parameter ADD		= 3'b011;
 	parameter REG_WRITE	= 3'b100;
+
+	integer i;
+
+	genvar k;
+	generate
+		for(k=0; k<16; k=k+1) begin: OUTPUT_SORTING
+			assign o_register[16*k +: 16] = register[k];
+		end
+	endgenerate
 	
 	
 	reg [2:0] current_state;
@@ -51,29 +60,16 @@ module SpMV_core(
 			REG_WRITE: begin
 				if(o_done)	next_state <= IDLE;
 				else		next_state <= MUL;
+			end
 			default: 		next_state <= IDLE;
 		endcase
 	end
-
-
-	wire o_done;
-	wire reg_write_done;
-
-	assign o_done = reg_write_done;
-
-	reg [15:0] register [0:15];
 	
-	genvar k;
-	generate
-		for(k=0; k<16; k=k+1) begin: OUTPUT_SORTING
-			assign o_register[16*k +: 16] = register[k];
-		end
-	endgenerate
-			
-	integer i;	
-
+	assign o_done = ((count != 8'b0)&&(count[3:0] == 4'b0)) ? 1'b1: 1'b0;
+	
+	reg [15:0] register [0:15];
 	reg [3:0] reg_addr;
-	wire done;
+
 	wire [15:0] mul_result;
 	wire [15:0] reg_result = (current_state == REG_READ)? register[reg_addr]: 16'b0;
 
@@ -87,15 +83,6 @@ module SpMV_core(
 
 		.result(mul_result)
 	);
-
-
-	SpMV_reg_read reg_read(
-		.i_clk(i_clk),
-		.i_rstn(i_rstn),
-		.i_reg_addr(i_reg_addr),
-
-		.o_reg_data(o_reg_data)
-	);
 	
 	SpMV_fp16_add addition(
 		.i_clk(i_clk),
@@ -106,22 +93,14 @@ module SpMV_core(
 		.result(out)
 	);
 
-	SpMV_reg_write reg_write(
-		.i_clk(i_clk),
-		.i_rstn(i_rstn),
-		.i_reg_data(i_reg_data)
-
-		.o_reg_write_done(reg_write_done)
-		);
-
-
 	always @(posedge i_clk, negedge i_rstn) begin
 		if(!i_rstn) begin
 			for(i=0; i<16; i=i+1) begin
 				register[i] <= 16'b0;
 			end
 		end else begin
-			register[reg_addr] <= out;
+			if(current_state == REG_WRITE)  register[reg_addr] <= out;
+			else							register[reg_addr] <= register[reg_addr];
 		end
 	end
 
